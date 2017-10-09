3fbd34d2824b9c97a42c42ae619984c8590debc1
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-10-09 18:43:04 -0400

    Add creature naming

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Creature : MonoBehaviour {
+
+    public string nickname;
+    public int happiness;
+    public TextMesh nameTag;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NameMe(string newName)
+    {
+        nickname = newName;
+        nameTag.text = newName;
+    }
+}
---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroController : MonoBehaviour {
+
+    public InputField input;
+    public Button button;
+    public Creature creature;
+
+	// Use this for initialization
+	void Start () {
+        StartNaming();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartNaming ()
+    {
+        input.gameObject.SetActive(true);
+    }
+
+    public void EndNaming ()
+    {
+        if (input.text != "")
+        {
+            creature.NameMe(input.text);
+            Destroy(input.gameObject);
+            Destroy(button.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
